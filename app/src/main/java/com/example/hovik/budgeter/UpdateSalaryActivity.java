cf8b66c74bf2ea1dba01a6242421a525c6c66b47
SandManTaj <38842116+SandManTaj@users.noreply.github.com>
2018-05-08 11:09:19 -0700

    finished update salary

--- app/src/main/java/com/example/hovik/budgeter/UpdateSalaryActivity.java
+++ app/src/main/java/com/example/hovik/budgeter/UpdateSalaryActivity.java
@@ -1,5 +1,6 @@
 package com.example.hovik.budgeter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -76,8 +77,55 @@ public class UpdateSalaryActivity extends AppCompatActivity {
                     float newpercarecur = newpercare - personalCarecostdif;
                     float newhealthcare = oldhealthCarecost * newRatio;
                     float newhealthcarecur = newhealthcare - healthCarecostdif;
+                    Intent infointent = new Intent(UpdateSalaryActivity.this, InfoActivity.class);
+                    infointent.putExtra("income", Float.parseFloat(SalaryText.getText().toString()));
+                    infointent.putExtra("food", newfood);
+                    infointent.putExtra("util", newutil);
+                    infointent.putExtra("housing", newhousing);
+                    infointent.putExtra("debt", newdebt);
+                    infointent.putExtra("savings", newsavings);
+                    infointent.putExtra("personalCare", newpercare);
+                    infointent.putExtra("entertainment", newentertainment);
+                    infointent.putExtra("healthCare", newhealthcare);
+                    infointent.putExtra("incomeCopy", Float.parseFloat(SalaryText.getText().toString()));
+                    infointent.putExtra("foodCopy", newfoodcur);
+                    infointent.putExtra("utilCopy", newutilcur);
+                    infointent.putExtra("housingCopy", newhousingcur);
+                    infointent.putExtra("debtCopy", newdebtcur);
+                    infointent.putExtra("savingsCopy", newsavingscur);
+                    infointent.putExtra("personalCareCopy", newpercarecur);
+                    infointent.putExtra("entertainmentCopy", newentertainmentcur);
+                    infointent.putExtra("healthCareCopy", newhealthcarecur);
 
+                    final Float surplus = Float.parseFloat(SalaryText.getText().toString()) - newfood
+                            - newutil - newhousing
+                            - newdebt - newsavings
+                            - newpercare - newentertainment
+                            - newhealthcare;
+                    final Float surplusPerc = (surplus / Float.parseFloat(SalaryText.getText().toString())) * 100;
+                    infointent.putExtra("surplus", Float.parseFloat(String.valueOf(surplusPerc)));
+                    infointent.putExtra("surplusCopy", Float.parseFloat(String.valueOf(surplusPerc)));
 
+                    startActivity(infointent);
+                    finish();
+                }
+
+                else
+                {
+                    Intent infointent = new Intent(UpdateSalaryActivity.this, InfoActivity.class);
+                    infointent.putExtra("income", Float.parseFloat(SalaryText.getText().toString()));
+                    infointent.putExtra("incomeCopy", Float.parseFloat(SalaryText.getText().toString()));
+                    final Float surplus = Float.parseFloat(SalaryText.getText().toString()) - foodcost
+                            - utilcost - housingcost
+                            - debtcost - savingscost
+                            - personalCarecost - entertainmentcost
+                            - healthCarecost;
+                    final Float surplusPerc = (surplus / Float.parseFloat(SalaryText.getText().toString())) * 100;
+                    infointent.putExtra("surplus", Float.parseFloat(String.valueOf(surplusPerc)));
+                    infointent.putExtra("surplusCopy", Float.parseFloat(String.valueOf(surplusPerc)));
+
+                    startActivity(infointent);
+                    finish();
 
                 }
 
