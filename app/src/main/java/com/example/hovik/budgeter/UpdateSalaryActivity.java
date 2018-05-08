cac2fd0cde448f70105b1fe6b6bdec4d61904c48
YIMBZ <31938596+YIMBZ@users.noreply.github.com>
2018-05-07 18:23:31 -0700

    made small changes to update salary

--- app/src/main/java/com/example/hovik/budgeter/UpdateSalaryActivity.java
+++ app/src/main/java/com/example/hovik/budgeter/UpdateSalaryActivity.java
@@ -2,6 +2,8 @@ package com.example.hovik.budgeter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UpdateSalaryActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class UpdateSalaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_salary);
+
+        final Button finishbtn = (Button) findViewById(R.id.FinishNewSalaryButton);
+        finishbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
